748a7ec3ffdf632019090e5dec1e597a382088b2
Zafarbek <przafar@mail.ru>
2021-07-30 14:19:17 +0500

    Version 2.0

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,13 @@
-$(document).ready(function() {
-	// $('body').hide()
-})
+$(document).ready(function () {
+  // $('body').hide()
+});
+$(document).ready(function () {
+  $(".next").click(function () {
+    $(".pagination").find(".pageNumber.active").next().addClass("active");
+    $(".pagination").find(".pageNumber.active").prev().removeClass("active");
+  });
+  $(".prev").click(function () {
+    $(".pagination").find(".pageNumber.active").prev().addClass("active");
+    $(".pagination").find(".pageNumber.active").next().removeClass("active");
+  });
+});
