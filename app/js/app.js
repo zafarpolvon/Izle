2d086bbde60a2c52d88edcadf4b2ab48070315dd
Zafarbek <przafar@mail.ru>
2021-08-02 15:29:32 +0500

    test

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,11 @@
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
 $(document).ready(function () {
   // $('body').hide()
 });
